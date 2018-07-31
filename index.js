d8e8c06c9f5a8da9da17a05c4708075e6276c523
huarxia <liubiao0810@live.cn>
2018-07-31 17:59:27 +0800

    backup brew cask and brew mas list

--- index.js
+++ index.js
@@ -3,14 +3,40 @@
 var name = process.argv[2];
 require('shelljs/global');
 const chalk = require('chalk');
+
 // brew is installed？
 const brew = which('brew');
 
 if (!brew) {
     console.log(chalk.red('没有安装 ') + chalk.bgRed('brew'));
     console.log(chalk.green('请复制 ') + chalk.yellow('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'));
+    return;
 } else {
-    console.log(chalk.green('已安装 brew 😀'));
+    console.log(chalk.green('本机已安装 brew 😀, 软件安装列表如下'));
     let brewList = exec('brew list').stdout;
-    console.log(chalk.green(brewList));
+    console.log('-------------------------');
+}
+
+// brew cask is installed？
+const brewCask = exec('brew cask --version');
+
+if (!brewCask) {
+    console.log(chalk.red('没有安装 ') + chalk.bgRed('brew cask'));
+    console.log(chalk.green('请复制运行：') + chalk.yellow('brew install cask'));
+} else {
+    console.log(chalk.green('本机已安装 brew cask 😀, 软件安装列表如下'));
+    let caskList = exec('brew cask list').stdout;
+    console.log('-------------------------');
+}
+
+// brew cask is installed？
+const mas = which('mas');
+
+if (!mas) {
+    console.log(chalk.red('没有安装 ') + chalk.bgRed('mas'));
+    console.log(chalk.green('请复制运行：') + chalk.yellow('brew install mas'));
+} else {
+    console.log(chalk.green('本机已安装 mas 😀, 软件安装列表如下'));
+    let caskList = exec('mas list').stdout;
+    console.log('-------------------------');
 }
